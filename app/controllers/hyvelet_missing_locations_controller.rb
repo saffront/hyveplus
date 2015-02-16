82d82a3e49a2624570c4fd47e612805b10c915d2
kaizenx <tzuchjeh.wu@vltkl.com>
2015-02-16 18:06:20 +0800

    removed default crap from the controllers

--- app/controllers/hyvelet_missing_locations_controller.rb
+++ app/controllers/hyvelet_missing_locations_controller.rb
@@ -1,65 +1,7 @@
 class HyveletMissingLocationsController < ApplicationController
   before_action :set_hyvelet_missing_location, only: [:show, :edit, :update, :destroy]
 
-  # GET /hyvelet_missing_locations
-  # GET /hyvelet_missing_locations.json
-  def index
-    @hyvelet_missing_locations = HyveletMissingLocation.all
-  end
-
-  # GET /hyvelet_missing_locations/1
-  # GET /hyvelet_missing_locations/1.json
-  def show
-  end
-
-  # GET /hyvelet_missing_locations/new
-  def new
-    @hyvelet_missing_location = HyveletMissingLocation.new
-  end
-
-  # GET /hyvelet_missing_locations/1/edit
-  def edit
-  end
-
-  # POST /hyvelet_missing_locations
-  # POST /hyvelet_missing_locations.json
-  def create
-    @hyvelet_missing_location = HyveletMissingLocation.new(hyvelet_missing_location_params)
-
-    respond_to do |format|
-      if @hyvelet_missing_location.save
-        format.html { redirect_to @hyvelet_missing_location, notice: 'Hyvelet missing location was successfully created.' }
-        format.json { render :show, status: :created, location: @hyvelet_missing_location }
-      else
-        format.html { render :new }
-        format.json { render json: @hyvelet_missing_location.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /hyvelet_missing_locations/1
-  # PATCH/PUT /hyvelet_missing_locations/1.json
-  def update
-    respond_to do |format|
-      if @hyvelet_missing_location.update(hyvelet_missing_location_params)
-        format.html { redirect_to @hyvelet_missing_location, notice: 'Hyvelet missing location was successfully updated.' }
-        format.json { render :show, status: :ok, location: @hyvelet_missing_location }
-      else
-        format.html { render :edit }
-        format.json { render json: @hyvelet_missing_location.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /hyvelet_missing_locations/1
-  # DELETE /hyvelet_missing_locations/1.json
-  def destroy
-    @hyvelet_missing_location.destroy
-    respond_to do |format|
-      format.html { redirect_to hyvelet_missing_locations_url, notice: 'Hyvelet missing location was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- app/controllers/master_pins_controller.rb
+++ app/controllers/master_pins_controller.rb
@@ -1,65 +1,7 @@
 class MasterPinsController < ApplicationController
   before_action :set_master_pin, only: [:show, :edit, :update, :destroy]
 
-  # GET /master_pins
-  # GET /master_pins.json
-  def index
-    @master_pins = MasterPin.all
-  end
-
-  # GET /master_pins/1
-  # GET /master_pins/1.json
-  def show
-  end
-
-  # GET /master_pins/new
-  def new
-    @master_pin = MasterPin.new
-  end
-
-  # GET /master_pins/1/edit
-  def edit
-  end
-
-  # POST /master_pins
-  # POST /master_pins.json
-  def create
-    @master_pin = MasterPin.new(master_pin_params)
-
-    respond_to do |format|
-      if @master_pin.save
-        format.html { redirect_to @master_pin, notice: 'Master pin was successfully created.' }
-        format.json { render :show, status: :created, location: @master_pin }
-      else
-        format.html { render :new }
-        format.json { render json: @master_pin.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /master_pins/1
-  # PATCH/PUT /master_pins/1.json
-  def update
-    respond_to do |format|
-      if @master_pin.update(master_pin_params)
-        format.html { redirect_to @master_pin, notice: 'Master pin was successfully updated.' }
-        format.json { render :show, status: :ok, location: @master_pin }
-      else
-        format.html { render :edit }
-        format.json { render json: @master_pin.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /master_pins/1
-  # DELETE /master_pins/1.json
-  def destroy
-    @master_pin.destroy
-    respond_to do |format|
-      format.html { redirect_to master_pins_url, notice: 'Master pin was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  
 
   private
     # Use callbacks to share common setup or constraints between actions.
