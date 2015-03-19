69160276ff705fdd7c2d5a00f1d396eef5d0f0e8
Jun Wan Goh <gohjunwan@gmail.com>
2015-03-19 19:26:20 +0800

    Correct belongs_to to has_one for HyveletSerializer

--- app/serializers/hyvelet_serializer.rb
+++ app/serializers/hyvelet_serializer.rb
@@ -2,6 +2,5 @@ class HyveletSerializer < ActiveModel::Serializer
 
   attributes :id, :name, :pin, :lat, :lng, :created_at, :updated_at, :user_id, :status, :distance, :uuid, :image
  
-	belongs_to :user
-	#has_many :hyvelet_missing_locations
+  has_one :user
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,5 +2,4 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :password, :password_confirmation, :created_at, :updated_at, :reset_password_token, :reset_password_token_expires_at, :reset_password_email_sent_at, :activation_state, :activation_token, :activation_token_expires_at, :username, :avatar, :first_name, :last_name, :subscription
 
   has_many :hyvelets
-  #has_many :authentications
 end
