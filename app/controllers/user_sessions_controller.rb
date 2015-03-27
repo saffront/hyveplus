bd244f4c01da53d3fd46c2e3011a83a2f0f06788
Jun Wan Goh <gohjunwan@gmail.com>
2015-03-27 18:40:09 +0800

    Fix user_sessions redirect path after logging in

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,7 @@ class UserSessionsController < ApplicationController
 
   def create
     if @user = login(params[:user_sessions][:email], params[:user_sessions][:password])
-      redirect_back_or_to(@user, notice: "Login successful")
+      redirect_back_or_to(my_account_path, notice: "Login successful")
     else
       redirect_to :back, alert: "Login failed. Please input correct email or password."
     end
