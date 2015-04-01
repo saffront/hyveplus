ab74ddb2a0ce469b262559661685e59a504f535b
Jun Wan Goh <gohjunwan@gmail.com>
2015-04-01 14:43:52 +0800

    [WIP] Initial implementation of product page

---
+++ app/assets/javascripts/application/shared/prettyPhoto.coffee
@@ -0,0 +1,6 @@
+$(document).ready ->
+  $('a[rel^=\'prettyPhoto\']').prettyPhoto
+    default_width: "100%"
+    default_height: "100%"
+    social_tools: ""
+  return
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@ end
 @uid = generate_uid
 @admin.authentications.build(provider: "email", uid: "#{@uid}").save!
 
-@image1 = File.join(Rails.root.join('app', 'assets', 'images', 'seeds', 'morgan_freeman.jpg'))
+@image1 = File.join(Rails.root.join('app', 'assets', 'images', 'db_seeds', 'morgan_freeman.jpg'))
 @user1 = User.create!(email: "morgan.freeman@hyve.com", username: "morgan-freeman", first_name: "Morgan", last_name: "Freeman", password: "password", password_confirmation: "password", avatar: open("#{@image1}"))
 @uid = generate_uid
 @user1.authentications.build(provider: "email", uid: "#{@uid}").save!
 @user1.hyves.build(name: "m.freeman-laptop", uuid: "hyve-0001", distance: "10").save!
 @user1.hyves.build(name: "m.freeman-phone", uuid: "hyve-0002", distance: "5").save!
 
-@image2 = File.join(Rails.root.join('app', 'assets', 'images', 'seeds', 'taylor_swift.jpg'))
+@image2 = File.join(Rails.root.join('app', 'assets', 'images', 'db_seeds', 'taylor_swift.jpg'))
 @user2 = User.create!(email: "taylor.swift@hyve.com", username: "taylor-swift", first_name: "Taylor", last_name: "Swift", password: "password", password_confirmation: "password", avatar: open("#{@image2}"))
 @uid = generate_uid
 @user2.authentications.build(provider: "email", uid: "#{@uid}").save!
