d87ba94b3e6ef501911085fc5a439144ec813c95
Jun Wan Goh <gohjunwan@gmail.com>
2015-04-02 18:32:07 +0800

    [API] Refactor user_sessions_controller

--- app/controllers/api/v1/user_sessions_controller.rb
+++ app/controllers/api/v1/user_sessions_controller.rb
@@ -2,10 +2,11 @@ class Api::V1::UserSessionsController < Api::ApiController
   skip_before_action :authenticate_token
 
   def create
-    if info_params[:provider] == "email"
+    case info_params[:provider]
+    when "email"
       @user = login(info_params[:email], info_params[:password])
       find_user(@user)
-    elsif info_params[:provider] == "facebook" || info_params[:provider] == "google"
+    when "facebook", "google"
       @user = User.find_by(email: info_params[:email]) 
       @auth = @user.try(:authentications).try(:find_by_uid, info_params[:uid]) #returns nil rather than raising an exception
       find_user(@user, @auth)
@@ -32,7 +33,6 @@ class Api::V1::UserSessionsController < Api::ApiController
 
   def register_new_user
     @user = User.new do |user|
-      #Complex
       user.email = info_params[:email]
       user.password = info_params[:password]
       user.password_confirmation = info_params[:password_confirmation]
@@ -42,14 +42,7 @@ class Api::V1::UserSessionsController < Api::ApiController
       user.last_name = info_params[:last_name]
       set_authentication(user)
     end
-
-    if @user.save
-      @user.generate_api_token!
-      render json: { user_session: UserSerializer.new(@user), api_token: @user.api_token }
-    else
-      render json: { errors: @user.errors }
-    end
-
+    save_and_generate_api_token(@user)
   end
 
   def set_authentication(user)
@@ -75,6 +68,15 @@ class Api::V1::UserSessionsController < Api::ApiController
       end while Authentication.exists?(uid: auth[:uid])
     else
       auth.uid = info_params[:uid]
+    end
+  end
+
+  def save_and_generate_api_token(user)
+    if user.save
+      user.generate_api_token!
+      render json: { user_session: UserSerializer.new(user), api_token: user.api_token }
+    else
+      render json: { errors: user.errors }
     end
   end
 end
