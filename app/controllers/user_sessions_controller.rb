58a9d1c9417afb18c1c73c74cb71d8af2afdbeab
Jun Wan Goh <gohjunwan@gmail.com>
2015-04-02 16:50:22 +0800

    Resize banner; remove flash message position: absolute; logout redirects to home page

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -16,6 +16,6 @@ class UserSessionsController < ApplicationController
   def destroy
     logout
     flash[:notice] = "Logged out"
-    redirect_to(request.referrer || root_path)
+    redirect_to root_path
   end
 end
